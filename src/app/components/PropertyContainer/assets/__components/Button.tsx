065e921b7fb860bb0e8c616a83e3e47aa2dfe26b
jszama <jmszamotulski@gmail.com>
2025-03-18 00:46:43 +0000

    added disabled option to Button component so user can not spam

--- src/app/components/PropertyContainer/assets/__components/Button.tsx
+++ src/app/components/PropertyContainer/assets/__components/Button.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 interface ButtonProps {
-    text: string;
-    type?: 'button' | 'submit' | 'reset';
-    onClick: () => void;
+  text: string;
+  type?: 'button' | 'submit' | 'reset';
+  onClick: () => void;
+  disabled: boolean;
 }
 
-export default function Button( { text, type, onClick }: ButtonProps ) {
+export default function Button( { text, type, onClick, disabled }: ButtonProps ) {
   return (
-      <button className="bg-orange-700 text-white hover:bg-orange-800 hover:drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] px-4 py-1 w-auto rounded-sm cursor-pointer transition-all duration-150 ease-in-out" type={type} onClick={onClick}>
+    <button className={`bg-orange-700 text-white hover:bg-orange-800 hover:drop-shadow-[0_2px_4px_rgba(0,0,0,0.2)] px-4 py-1 w-auto rounded-sm transition-all duration-150 ease-in-out ${disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`} type={type} onClick={onClick} disabled={disabled}>
         {text}
     </button>
   )
