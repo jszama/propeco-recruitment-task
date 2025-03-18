42a20068767d75bb351d65ff8e80d55bd401202c
jszama <jmszamotulski@gmail.com>
2025-03-18 00:11:10 +0000

    created PropertyData component which holds all property data related elements

---
+++ src/app/components/PropertyContainer/assets/__components/PropertyData/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropertyDataList from './assets/PropertyDataList'
+import Button from '../Button';
+
+interface PropertyDataProps {
+  rawData: any;
+  onSubmit: any;
+}
+
+export default function PropertyData({ rawData, onSubmit }: PropertyDataProps) {
+  return (
+    <article className='flex flex-col gap-4 justify-between p-2 items-start w-auto h-auto relative'>
+      <h2 className='text-3xl font-semibold'>Property Data</h2>
+      <PropertyDataList rawData={rawData} />
+      <Button
+        type="button"
+        text="Back to Search"
+        onClick={() => {onSubmit(null)}}
+      />
+    </article>
+  );
+}
