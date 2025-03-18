9274865e53bcc5be21924d8b99f6511953005ebb
jszama <jmszamotulski@gmail.com>
2025-03-18 00:09:27 +0000

    created DataHeader component to hold category title for property data

---
+++ src/app/components/PropertyContainer/assets/__components/PropertyData/assets/DataHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface DataHeaderProps {
+    title: string;
+}
+
+export default function DataHeader({ title }: DataHeaderProps) {
+  return (
+      <header className='text-2xl font-semibold'>{title}</header>
+  )
+}
